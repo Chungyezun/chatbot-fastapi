2e4142f3f4b576073e0f63472519b11811dfe6f2
Chungyezun <maple0729@kaist.ac.kr>
2024-08-23 20:14:57 +0900

    fastapi practicing & add requirements.txt

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
-@app.get("/")
-def 이름():
-  return '보낼 값'
+class ModelName(str, Enum):
+  alexnet = "alexnet"
+  resnet = "resnet"
+
+@app.get("/home/{name}")
+def read_name(name:str):
+  return {"name": name}
+
+@app.get("/home/{name}")
+def read_name_error(name:int):
+  return {"name": name}
+
+@app.get("/models/{model_name}")
+def get_model(model_name:ModelName):
+  if model_name is ModelName.alexnet:
+    return {"model_name": model_name, "message": "Alexnet!"}
+  if model_name.value == "resnet":
+    return {"model_name": model_name, "message": "Resnet!"}
